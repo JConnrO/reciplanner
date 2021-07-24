7d5d7f0bbcdb8741d7ab40e3b7e197fb57ae2c33
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-24 19:33:42 -0400

    user routes put - update user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,4 +63,28 @@ router.post('/', (req, res) => {
         })
 })
 
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
