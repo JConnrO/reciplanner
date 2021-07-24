b524e107d190974dc3fd1ce68771c2b670274de5
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-24 19:38:21 -0400

    package user route

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const path = require('path');
-// const routes = require('./routes');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
 const exphbs = require("express-handlebars");
 // Requires the 'express-session' module
 // const session = require(`express-session`);
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -27,7 +17,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
