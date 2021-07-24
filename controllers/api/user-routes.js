f105ba0c31f45f09b0fe22391f1ef5c4018d2af4
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-24 19:34:23 -0400

    user route destroy - delete user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -87,4 +87,24 @@ router.put('/:id', (req, res) => {
         });
 });
 
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
