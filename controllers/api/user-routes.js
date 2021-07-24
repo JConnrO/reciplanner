1d8fe6f1482bdb9e92db7c76df7db49e9e072506
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-24 19:31:05 -0400

    user route get one user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,4 +12,37 @@ router.get('/', (req, res) => {
         });
 });
 
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
 module.exports = router;
