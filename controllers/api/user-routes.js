7d03d5f734258b89cee3e7bba23b0ae8bea54425
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-24 19:29:15 -0400

    user route get all

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method)
+    User.findAll()
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
