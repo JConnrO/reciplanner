f2b50c3c6707b2ee6cbab2c07c4528330c1696d3
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-24 19:32:35 -0400

    user route post - create user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -44,5 +44,23 @@ router.get('/:id', (req, res) => {
         });
 });
 
+// POST /api/users
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
+})
 
 module.exports = router;
