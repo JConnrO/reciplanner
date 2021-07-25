f0438528a19017ee0ca9e09881952404a8ceb7fc
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-07-25 14:27:20 -0400

    routes turned on in server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routes = require('./routes');
 const path = require('path');
 const sequelize = require('./config/connection');
 const exphbs = require("express-handlebars");
@@ -20,6 +21,9 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
