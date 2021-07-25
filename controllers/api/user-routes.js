52c78006a35eb0853d63c29636dcfaa352c539d9
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-25 11:28:14 -0400

    login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -51,6 +51,28 @@ router.post('/', (req, res) => {
         });
 })
 
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
--- models/User.js
+++ models/User.js
@@ -3,7 +3,10 @@ const sequelize = require('../config/connection');
 
 // create our User model
 class User extends Model {
-
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
 }
 
 // create fields/columns for User model
