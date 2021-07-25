0caa4427503f11bbee0f33b4eb0b493745915d50
Maame Konadu Assasie-Gymah <maame@Maames-MacBook-Pro.local>
2021-07-25 14:18:41 -0400

    vote id requirements added to vote model

--- models/Vote.js
+++ models/Vote.js
@@ -2,3 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Vote extends Model {}
+
+Vote.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      }
+    })
