fad8ad2a9173eae7218dc628c6aaba76ddb32ef2
Imaparadox <stevemarsh117@gmail.com>
2021-07-25 12:24:42 -0400

    Added logic to login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,21 @@
 // UNDER CONSTRUCTION
 async function loginHandler(event) {
-    console.log();
+    event.preventDefault();
+
+    const userEmail = document.querySelector('.email-login').value.trim();
+    const userPassword = document.querySelector('.password-login').value.trim();
+
+    if (userEmail && userPassword) {
+        // Need route to be created to fetch
+        const userResponse = await fetch('', {
+            method: 'post',
+            body: JSON.stringify({
+                userEmail,
+                userPassword
+            }),
+            headers: {'Content-'}
+        })
+
+    }
+
 };
