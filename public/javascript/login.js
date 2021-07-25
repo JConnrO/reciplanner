2f265694104f0d46b9fb2030e9bb36004f7bdb1f
Imaparadox <stevemarsh117@gmail.com>
2021-07-25 12:41:24 -0400

    commented out code

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,21 +1,21 @@
 // UNDER CONSTRUCTION
-async function loginHandler(event) {
-    event.preventDefault();
+// async function loginHandler(event) {
+//     event.preventDefault();
 
-    const userEmail = document.querySelector('.email-login').value.trim();
-    const userPassword = document.querySelector('.password-login').value.trim();
+//     const userEmail = document.querySelector('.email-login').value.trim();
+//     const userPassword = document.querySelector('.password-login').value.trim();
 
-    if (userEmail && userPassword) {
-        // Need route to be created to fetch
-        const userResponse = await fetch('', {
-            method: 'post',
-            body: JSON.stringify({
-                userEmail,
-                userPassword
-            }),
-            headers: {'Content-Type': ''}
-        });
+//     if (userEmail && userPassword) {
+//         // Need route to be created to fetch
+//         const userResponse = await fetch('', {
+//             method: 'post',
+//             body: JSON.stringify({
+//                 userEmail,
+//                 userPassword
+//             }),
+//             headers: {'Content-Type': 'application/json'}
+//         });
 
-    }
+//     }
 
-};
+// };
