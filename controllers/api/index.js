e09726332c941476e5d9aca4b0d264253b9c7abe
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-07-25 20:27:08 -0400

    corrected user backend routes to display table in db using MYSQL workbench

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,13 @@
 const router = require('express').Router();
 
-// const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes.js');
 // const recipeRoutes = require('./recipe-routes');
+
+router.use('/users', userRoutes);
+// router.use('/recipes', recipeRoutes);
 
 //dummy data 
 const apiRoutes = require('./book-routes');
-
-// router.use('/users', userRoutes);
-
-// router.use('/recipes', recipeRoutes);
-
 //dummy data
 router.use('/api', apiRoutes);
 
