1caaaac5ff5f7582536c190fb6bb7eb2d07c9801
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-07-25 20:21:02 -0400

    commented out recipe and user routes, not working, dummy data of books worked to display table in mysql workbench, will work with backend to correct

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,14 @@ const router = require('express').Router();
 // const userRoutes = require('./user-routes.js');
 // const recipeRoutes = require('./recipe-routes');
 
-// router.use('/users', userRoutes);
-// router.use('/recipes', recipeRoutes);
-
+//dummy data 
 const apiRoutes = require('./book-routes');
 
+// router.use('/users', userRoutes);
 
+// router.use('/recipes', recipeRoutes);
+
+//dummy data
 router.use('/api', apiRoutes);
 
 module.exports = router;
