562aba69e0ecbf3ab4bee5f83910495304798c77
Imaparadox <stevemarsh117@gmail.com>
2021-07-26 13:51:43 -0400

    Created functions

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,21 +1,54 @@
 // UNDER CONSTRUCTION
-// async function loginHandler(event) {
-//     event.preventDefault();
+async function loginHandler(event) {
+    event.preventDefault();
 
-//     const userEmail = document.querySelector('.email-login').value.trim();
-//     const userPassword = document.querySelector('.password-login').value.trim();
+    const userEmail = document.querySelector('.email-login').value.trim();
+    const userPassword = document.querySelector('.password-login').value.trim();
 
-//     if (userEmail && userPassword) {
-//         // Need route to be created to fetch
-//         const userResponse = await fetch('', {
-//             method: 'post',
-//             body: JSON.stringify({
-//                 userEmail,
-//                 userPassword
-//             }),
-//             headers: {'Content-Type': 'application/json'}
-//         });
+    if (userEmail && userPassword) {
+        // Need route to be to be fetched
+        const userResponse = await fetch('/api/user', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (userResponse.ok) {
+            //Replace dashboard
+            document.location.replace('');
+        } else {
+            alert(userResponse.statusText);
+        }
+    }
+};
 
-//     }
+async function signupHandler(event) {
+    event.preventDefault();
 
-// };
+    const emailSignup = document.querySelector('.email-signup').value.trim();
+    const passwordSignup = document.querySelector('.password-signup').value.trim();
+
+    if (emailSignup && passwordSignup) {
+        // Need route to be to be fetched
+        const userResponse = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (userResponse.ok) {
+            //Replace dashboard
+            document.location.replace('/dashboard/');
+        } else {
+            alert(userResponse.statusText);
+        }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupHandler);
