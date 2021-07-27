167428d1218c7918016cd7714c5429d84484dc2a
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-07-26 22:13:10 -0400

    commented out routes to test funcationality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,119 +1,19 @@
-<<<<<<< HEAD
-const router = require("express").Router();
-const { Recipe, User, Vote } = require('../models');
-const sequelize = require('../config/connection');
-=======
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Recipe, User, Vote } = require('../models');
 
 // get all recipes for homepage
-router.get('/', (req, res) => {
-  console.log('======================');
-  Recipe.findAll({
-    attributes: [
-      'id',
-      'youtube_url',
-      'title',
-      'description',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const recipes = dbPostData.map(recipe => recipe.get({ plain: true }));
-
-      res.render('landing', {
-        recipes,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-// get single post
-router.get('/recipe/:id', (req, res) => {
-  Recipe.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'youtube_url',
-      'title',
-      'description',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-
-      const recipe = dbPostData.get({ plain: true });
-
-      res.render('single-post', {
-        recipe,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
-});
-
-module.exports = router;
-
->>>>>>> develop2
-// THE FOLLOWING ROUTES ARE FOR FRONTEND TESTING ONLY
-// Landing Page
-
-// DATA
-
-// Routes
-// =============================================================
-
 // router.get('/', (req, res) => {
-<<<<<<< HEAD
 //   console.log('======================');
 //   Recipe.findAll({
-//     // Query configuration
 //     attributes: [
-//      'id',
-//      'youtube_url', 
-//      'title', 
-//      'description',
-//      'created_at',
-//      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)'), 'vote_count']
+//       'id',
+//       'youtube_url',
+//       'title',
+//       'description',
+//       'created_at',
+//       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)'), 'vote_count']
 //     ],
-//     order: [['created_at', 'DESC']],
 //     include: [
 //       {
 //         model: User,
@@ -122,10 +22,10 @@ module.exports = router;
 //     ]
 //   })
 //     .then(dbPostData => {
-//       const posts = dbPostData.map(post => post.get({ plain: true }));
+//       const recipes = dbPostData.map(recipe => recipe.get({ plain: true }));
 
-//       res.render('homepage', {
-//         posts,
+//       res.render('landing', {
+//         recipes,
 //         loggedIn: req.session.loggedIn
 //       });
 //     })
@@ -134,40 +34,98 @@ module.exports = router;
 //       res.status(500).json(err);
 //     });
 // });
-router.get('/', (req, res) => {
-=======
->>>>>>> develop2
 
-//     // Send all of the books to 'index.handlebars' as an object
-//     const posts = [
-//         {
-//           title: 'Best Chicken Penne',
-//           link: 'https://www.youtube.com/watch?v=Qc2aPjIJk-8',
-//           description: 'Super Tasty',
-//           upvote: 4000,
-//           username: 'ssss'
-//         },
-//         {
-//           title: 'Amazing Chicken Marsala',
-//           link: 'https://www.youtube.com/watch?v=AWNU1OccN5Q',
-//           description: 'Much wow',
-//           upvote: 2000,
-//           username: 'ssss'
-//         },
-//         {
-//           title: 'Super simple Sushi',
-//           link: "https://www.youtube.com/watch?v=joweUxpHaqc",
-//           description: 'No way!',
-//           upvote: 4,
-//           username: 'ssss'
-//         }
-//       ];
+// // get single post
+// router.get('/recipe/:id', (req, res) => {
+//   Recipe.findOne({
+//     where: {
+//       id: req.params.id
+//     },
+//     attributes: [
+//       'id',
+//       'youtube_url',
+//       'title',
+//       'description',
+//       'created_at',
+//       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)'), 'vote_count']
+//     ],
+//     include: [
+//       {
+//         model: User,
+//         attributes: ['username']
+//       }
+//     ]
+//   })
+//     .then(dbPostData => {
+//       if (!dbPostData) {
+//         res.status(404).json({ message: 'No post found with this id' });
+//         return;
+//       }
 
-//     const data = {
-//       cards: posts
-//     };
-//     res.render('landing', data);
+//       const recipe = dbPostData.get({ plain: true });
+
+//       res.render('single-post', {
+//         recipe,
+//         loggedIn: req.session.loggedIn
+//       });
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
 // });
+
+// router.get('/login', (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect('/');
+//     return;
+//   }
+
+//   res.render('login');
+// });
+
+// module.exports = router;
+
+// THE FOLLOWING ROUTES ARE FOR FRONTEND TESTING ONLY
+// Landing Page
+
+// DATA
+
+// Routes
+// =============================================================
+
+router.get('/', (req, res) => {
+
+    // Send all of the books to 'index.handlebars' as an object
+    const posts = [
+        {
+          title: 'Best Chicken Penne',
+          link: 'https://www.youtube.com/watch?v=Qc2aPjIJk-8',
+          description: 'Super Tasty',
+          upvote: 4000,
+          username: 'ssss'
+        },
+        {
+          title: 'Amazing Chicken Marsala',
+          link: 'https://www.youtube.com/watch?v=AWNU1OccN5Q',
+          description: 'Much wow',
+          upvote: 2000,
+          username: 'ssss'
+        },
+        {
+          title: 'Super simple Sushi',
+          link: "https://www.youtube.com/watch?v=joweUxpHaqc",
+          description: 'No way!',
+          upvote: 4,
+          username: 'ssss'
+        }
+      ];
+
+    const data = {
+      cards: posts
+    };
+    res.render('landing', data);
+});
 
 // //LOGIN dummy data SMM
 
