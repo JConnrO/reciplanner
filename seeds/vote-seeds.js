c66cd8a138d8385dc11af5189fc5ba434e2fd415
j.connor.obrien@gmail.com <j.connor.obrien@gmail.com>
2021-07-26 21:29:43 -0400

    vote seeds working

--- seeds/vote-seeds.js
+++ seeds/vote-seeds.js
@@ -1,54 +1,12 @@
-// const { Vote } = require('../models');
+const { Vote } = require('../models');
 
-// const votedata = [
-//   {
-//     user_id: 1,
-//     recipe_id: 4
-//   },
-//   {
-//     user_id: 5,
-//     recipe_id: 1
-//   },
-//   {
-//     user_id: 4,
-//     recipe_id: 5
-//   },
-//   {
-//     user_id: 3,
-//     recipe_id: 1
-//   },
-//   {
-//     user_id: 4,
-//     recipe_id: 1
-//   },
-//   {
-//     user_id: 3,
-//     recipe_id: 3
-//   },
-//   {
-//     user_id: 5,
-//     recipe_id: 5
-//   },
-//   {
-//     user_id: 1,
-//     recipe_id: 4
-//   },
-//   {
-//     user_id: 4,
-//     recipe_id: 5
-//   },
-//   {
-//     user_id: 5,
-//     recipe_id: 5
-//   },
-//   {
-//     user_id: 3,
-//     recipe_id: 5
-//   },
+const votedata = [
+  {
+    user_id: 1,
+    recipe_id: 4
+  }
+];
 
+const seedVotes = () => Vote.bulkCreate(votedata);
 
-// ];
-
-// const seedVotes = () => Vote.bulkCreate(votedata);
-
-// module.exports = seedVotes;
+module.exports = seedVotes;
