cfa1d9fc8b4b5df4fa0cf7f775a5a69aebcf5e42
j.connor.obrien@gmail.com <j.connor.obrien@gmail.com>
2021-07-26 20:26:50 -0400

    seeds index.js + files

---
+++ seeds/index.js
@@ -0,0 +1,19 @@
+const seedUsers = require('./user-seeds');
+const seedRecipes = require('./recipes-seeds');
+const seedVotes = require('./vote-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------');
+  await seedUsers();
+  console.log('--------------');
+  await seedRecipes();
+  console.log('--------------');
+  await seedVotes();
+  console.log('--------------');
+  process.exit(0);
+};
+
+seedAll();
