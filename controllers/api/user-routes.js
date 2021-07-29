b084e9a77e53d8860fe9b9b27e2426c9850aaf7f
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-28 22:36:18 -0400

    export auth

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 const bcrypt = require('bcrypt');
+const withAuth = require('../utils/auth');
 
 // GET /api/users
 router.get('/', (req, res) => {
