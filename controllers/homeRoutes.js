a98de138568c7671c075cc72385abc043ac041cc
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-07-29 18:00:55 -0400

    add edit route to homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -35,6 +35,7 @@ router.get('/', (req, res) => {
     });
 });
 
+// login route if user loggedin
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -44,121 +45,43 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+// route for dashboard
 router.get('/dashboard', (req,res) =>{
   res.render('dashboard')
 })
+
+//route for edit post
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      'youtube_url',
+      'title',
+      'description',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('dashboard', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 module.exports = router;
-// // get single post
-// router.get('/recipe/:id', (req, res) => {
-//   Recipe.findOne({
-//     where: {
-//       id: req.params.id
-//     },
-//     attributes: [
-//       'id',
-//       'youtube_url',
-//       'title',
-//       'description',
-//       'created_at',
-//       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)'), 'vote_count']
-//     ],
-//     include: [
-//       {
-//         model: User,
-//         attributes: ['username']
-//       }
-//     ]
-//   })
-//     .then(dbPostData => {
-//       if (!dbPostData) {
-//         res.status(404).json({ message: 'No post found with this id' });
-//         return;
-//       }
-
-//       const recipe = dbPostData.get({ plain: true });
-
-//       res.render('single-post', {
-//         recipe,
-//         loggedIn: req.session.loggedIn
-//       });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-// router.get('/login', (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
-
-//   res.render('login');
-// });
-
-// module.exports = router;
-
-// THE FOLLOWING ROUTES ARE FOR FRONTEND TESTING ONLY
-// Landing Page
-
-// DATA
-
-// Routes
-// =============================================================
-
-// router.get('/', (req, res) => {
-
-//     // Send all of the books to 'index.handlebars' as an object
-//     const posts = [
-//         {
-//           title: 'Best Chicken Penne',
-//           link: 'https://www.youtube.com/watch?v=Qc2aPjIJk-8',
-//           description: 'Super Tasty',
-//           upvote: 4000,
-//           username: 'ssss'
-//         },
-//         {
-//           title: 'Amazing Chicken Marsala',
-//           link: 'https://www.youtube.com/watch?v=AWNU1OccN5Q',
-//           description: 'Much wow',
-//           upvote: 2000,
-//           username: 'ssss'
-//         },
-//         {
-//           title: 'Super simple Sushi',
-//           link: "https://www.youtube.com/watch?v=joweUxpHaqc",
-//           description: 'No way!',
-//           upvote: 4,
-//           username: 'ssss'
-//         }
-//       ];
-
-//     const data = {
-//       cards: posts
-//     };
-//     res.render('landing', data);
-// });
-
-// //LOGIN dummy data SMM
-
-// router.get('/login', (req, res) => {
-//     const food = [
-//         {
-//           title: 'Love You Forever',
-//           read: false,
-//           author: 'Robert Munsch'
-//         },
-//         {
-//           title: 'The Giving Tree',
-//           read: false,
-//           author: 'Shel Silverstein'
-//         }
-//       ]
-
-//     const data = {
-//       library: food
-//     };
-//     res.render('login', data);
-// });
-
