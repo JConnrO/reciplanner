4a528ffbdb967b3b10d4460cef53e4fc063b8a6f
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-07-29 19:08:38 -0400

    functionality for edit of post

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -6,7 +6,7 @@ async function editFormHandler(event) {
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
-  const response = await fetch(`/api/posts/${id}`, {
+  const response = await fetch(`/api/recipes/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
       title,
