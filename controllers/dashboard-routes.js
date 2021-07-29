d2211effbe65e1578fc09e02241f3d37b8bf0f8f
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-28 22:42:47 -0400

    edit post route with auth

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -36,6 +36,41 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+//route to render edit-post.handlebars template 
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      'youtube_url',
+      'title',
+      'description',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 // get single post
 router.get('/recipe/:id', withAuth, (req, res) => {
   Recipe.findOne({
@@ -43,11 +78,11 @@ router.get('/recipe/:id', withAuth, (req, res) => {
       id: req.params.id
     },
     attributes: [
-        'id',
-        'youtube_url',
-        'title',
-        'description',
-        'created_at',
+      'id',
+      'youtube_url',
+      'title',
+      'description',
+      'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)'), 'vote_count']
     ],
     include: [
