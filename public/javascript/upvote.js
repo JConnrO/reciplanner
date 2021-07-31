19ea689bdf8c87775ad4f780c2643ecc6013acf3
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-07-30 20:05:47 -0400

    commented out upvote while working on it

--- public/javascript/upvote.js
+++ public/javascript/upvote.js
@@ -1,31 +1,31 @@
-// var vote= document.getElementById('upvoteCount')
-// var vote= 45
-async function upvoteClickHandler(event) {
+// // var vote= document.getElementById('upvoteCount')
+// // var vote= 45
+// async function upvoteClickHandler(event) {
   
-    event.preventDefault();
-  alert('working!');
-    const id= req.body.id
-    console.log(id)
+//     event.preventDefault();
+//   alert('working!');
+//     const id= req.body.id
+//     console.log(id)
 
-    const response = await fetch('/api/recipes/upvote', {
-      method: 'PUT',
-      body: JSON.stringify({
-        user_id: id
-        // recipe_id: recipe_id,
-        // vote_count: vote
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+//     const response = await fetch('/api/recipes/upvote', {
+//       method: 'PUT',
+//       body: JSON.stringify({
+//         user_id: id
+//         // recipe_id: recipe_id,
+//         // vote_count: vote
+//       }),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
   
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert(response.statusText);
-    }
-  }
+//     if (response.ok) {
+//       document.location.reload();
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
   
-  // select id of button and run function on click
-  document.querySelector('#upvote').addEventListener('click', upvoteClickHandler);
+//   // select id of button and run function on click
+//   document.querySelector('#upvote').addEventListener('click', upvoteClickHandler);
   
